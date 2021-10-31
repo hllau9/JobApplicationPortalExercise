3df6a98daf24bf4bd08ed3cd774615ee59b263dd
HLLau <hllau9@gmail.com>
2021-11-01 04:12:08 +0800

    enhanced aesthetics.

--- JobApplication/Models/JobPostingVM.cs
+++ JobApplication/Models/JobPostingVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace JobApplication.Web.Models
     public class JobPostingVM
     {
         public int Id { get; set; }
+        [Display(Name = "Job Title")] 
         public string JobTitle { get; set; }
+        [Display(Name = "Job Description")]
         public string JobDescription{ get; set; }
+        [Display(Name = "Employer")]
         public string Employer { get; set; }
         public List<string> SkillList { get; set; }
     }
